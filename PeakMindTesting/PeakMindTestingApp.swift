562881eec1d313f64af3ad2e9b5112c7923e534d
mikeyhalim <kechio.halim@gmail.com>
2024-02-09 23:23:20 -0800

    Initial Commit

---
+++ PeakMindTesting/PeakMindTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeakMindTestingApp.swift
+//  PeakMindTesting
+//
+//  Created by Mikey Halim on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PeakMindTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
